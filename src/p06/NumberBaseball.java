1dfaa9545e6dd6438874137e9c1acb0c1669064e
songkungsuk <songkungsug@gmail.com>
2023-07-03 18:02:44 +0900

    ㅇㅅㅇ

--- src/p06/NumberBaseball.java
+++ src/p06/NumberBaseball.java
@@ -11,10 +11,9 @@ public class NumberBaseball {
 		int[] baseball = new int[3];
 		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
-		int strike = 0;
-		int ball = 0;
-//		boolean condition = true;
-//		while(condition) {
+		
+		boolean condition = true;
+		
 			for(int i=0; i<baseball.length; i++) { //숫자 랜덤생성
 				baseball[i] = random.nextInt(10);
 				
@@ -24,40 +23,45 @@ public class NumberBaseball {
 					}
 				}
 			}
-			for(int i=0; i<baseball.length; i++) {//baseball 출력
-				System.out.print(baseball[i]);
-			}
-			System.out.println();
-//			if(baseball[0] == 0) {
-//				condition = false;
-//			}
-//		}
-		System.out.println("서로 다른 숫자 3자리를 입력하시오.");
-		int[] number = new int[3];
-		String input_number = scanner.nextLine();
-		for(int i=0; i<baseball.length; i++) {
-			number[i] = Integer.parseInt(input_number.substring(i, i+1));
-		}
+
 		
-		for(int i=0; i<baseball.length; i++) {
-			if(baseball[i] == number[i]) {
-				strike++;
+		while(condition) {
+			int strike = 0;
+			int ball = 0;
+		
+			System.out.println("서로 다른 숫자 3자리를 입력하시오.");
+			int[] number = new int[3];
+			String input_number = scanner.nextLine();
+			for(int i=0; i<baseball.length; i++) {
+				number[i] = Integer.parseInt(input_number.substring(i, i+1));
 			}
 			
-			for(int j=0; j<baseball.length; j++) {
-				 if(i != j && baseball[i] == number[j]) {
-					 ball++;
-				 }
+			for(int i=0; i<baseball.length; i++) {
+				if(baseball[i] == number[i]) {
+					strike++;
+				}
+				
+				for(int j=0; j<baseball.length; j++) {
+					 if(i != j && baseball[i] == number[j]) {
+						 ball++;
+					 }
+				}
+			}
+			
+
+			for(int i=0; i<baseball.length; i++) {
+				System.out.print(number[i]);
+			}
+			System.out.println();
+		
+			System.out.println("Ball : "+ ball + " Strike :" + strike );
+			if(strike == 3) {
+				for(int i=0; i<baseball.length; i++) {//baseball 출력
+					System.out.print(baseball[i]);
+				}
+				System.out.println("축하합니다 성공하셧습니다");
+				condition = false;
 			}
 		}
-		
-		
-		
-		for(int i=0; i<baseball.length; i++) {
-			System.out.print(number[i]);
-		}
-		System.out.println();
-	
-		System.out.println("Ball : "+ ball + " Strike :" + strike );
 	}
 }
